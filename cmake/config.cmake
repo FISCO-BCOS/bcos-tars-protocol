20cfeb18559aa051d59966250670619e2a8ff0ba
morebtcg <651932351@qq.com>
2021-10-28 14:20:18 +0800

    Add attribute field

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,5 +1,5 @@
 hunter_config(bcos-framework VERSION 3.0.0-local
-	URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/7ecf855fb074dd8e7ef8a2cd4896283afa950c46.tar.gz
-    SHA1 be187e0ecc3c5d834a0028724dcd331408930ecf
+	URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/8b6703fbaa4eb0123e8318b6ac68d2f9fa81ee35.tar.gz
+    SHA1 8ce684729163765f1cdae94badc5bf3ae52d9e19
 	CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
 )
