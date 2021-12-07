0558a9c29240ad148a82b401bd176620dcd640c3
cyjseagull <1547158861@qq.com>
2021-12-07 15:13:07 +0800

    update hunter (#40)

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -3,3 +3,4 @@ hunter_config(bcos-framework VERSION 3.0.0-local
 	SHA1 1fabfc8bb45d3793f5a4789a01f32fb35ec691e2
 	CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
 )
+hunter_config(tarscpp VERSION 3.0.3-7299ad23)
